79219477b7de5c83a6f3fecc09ad65a14b5e2bfe
Sublustrum007 <Sublustrum007@yandex.ru>
2023-10-14 16:10:20 +0300

    add Example 010 Method-Array

--- C#/lecture/Example010_MethodArry/Program.cs
+++ C#/lecture/Example010_MethodArry/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int [] array = { 1, 42, 42, 4, 18, 47, 79, 123, 456, 18};
+
+int size = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < size){
+    if (array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
