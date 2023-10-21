7e309fb675e9df0f3afef663c5685dd5d06979d2
Sublustrum007 <Sublustrum007@yandex.ru>
2023-10-22 00:31:42 +0300

    Add Task 3 Seminar 3

---
+++ C#/Seminar_3/Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿void ShowCube(int N){
+    int count = 1;
+    while(count <= N){
+        Console.WriteLine(Math.Pow(count, 3));
+        count++;
+    }
+    return;
+}
+
+
+Console.Write("Введите число `N`: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+ShowCube(N);  //Вызов метода 
