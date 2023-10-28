ba068c543709e8e9f68e48b06da6d74b2fe1501b
Sublustrum007 <Sublustrum007@yandex.ru>
2023-10-28 21:34:58 +0300

    Add Task 2 Seminar 4

---
+++ C#/Seminar_4/Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+string Promt(string str){
+    Console.Write(str);
+    string result = Console.ReadLine();
+    return result;
+}
+
+int[] CreateArray(string arg1){
+    int length = arg1.Length;
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++){       
+        array[i] = int.Parse(arg1[i] + "");
+    }
+    return array;
+}
+int SummArrayNumb(int[] arg1, string arg2){
+    int length = arg2.Length;
+    int summ = 0;
+    for (int i = 0; i < length; i++){
+        summ = summ + arg1[i];
+    }
+    return summ;
+
+}
+
+void PrintArray(int[] array){
+    Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+
+string str1 = Promt("Введите первое число: ");
+
+int[] arr = CreateArray(str1);
+Console.WriteLine(SummArrayNumb(arr, str1));
+
