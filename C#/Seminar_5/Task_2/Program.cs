d21c53605a752d2b57b2e24f45ce945854c62bdb
Sublustrum007 <Sublustrum007@yandex.ru>
2023-10-29 02:38:19 +0300

    Add Task 2 Seminar 5

---
+++ C#/Seminar_5/Task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+string Promt(string str){
+    Console.Write(str);
+    string result = Console.ReadLine();
+    return result;
+}
+
+void PrintArray(int[] array){
+    int length = array.Length;
+    Console.WriteLine("Массив:");
+    for (int i = 0; i < length - 1; i++){
+        Console.Write(array[i]+"\t");
+    }
+    Console.WriteLine(array[length - 1]);
+}
+
+int[] CreateArray(string str){
+    int length = Convert.ToInt32(str);
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++){
+        result[i] = new Random().Next(0, 100);
+    }
+    return result;
+}
+
+int SumOddElements(int[] array){
+    int length = array.Length;
+    int summ = 0;
+    for (int i = 1; i < length; i++){
+        if (i %2 != 0){
+        summ = summ + array[i];
+        }
+    }
+    return summ;
+
+}
+
+// string str = Promt("Введите количество элементов массива: ");
+// int[] array = CreateArray(str);
+int[] array = new int[] { 18, 76, 11};
+PrintArray(array);
+int sumOdd = SumOddElements(array);
+Console.WriteLine($"Сумма нечетных элементов: {sumOdd}");
