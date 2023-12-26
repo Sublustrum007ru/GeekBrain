7551cd778d50dcb54a1041e3a2c7ab8de4a41fb4
Sublustrum007 <Sublustrum007@yandex.ru>
2023-12-26 20:09:14 +0300

    26.12.2023

--- Python/.Python/testing.py
+++ Python/.Python/testing.py
@@ -1,10 +1,10 @@
-a = [1, 21 ,3, -46, 53, -6, 117]
-n = len(a)
-print(type(a))
-for k in range(n-1):
-    min = k
-    for i in range(k, n):
-        if a[i] < a[min]:
-            min = i
-    a[k], a[min] = a[min], a[k]
-print(a)
+list = [1, 21 ,3, -46, 53, -6, 117]
+size = len(list)
+print(type(list))
+for i in range(size-1):
+    min_position = i
+    for j in range(i, size):
+        if list[j] < list[min_position]:
+            min_position = j
+    list[i], list[min_position] = list[min_position], list[i]
+print(list)
