26adfc45242fe272dfb94c1ddbd52c8ab6f6ad32
Sublustrum007 <Sublustrum007@yandex.ru>
2023-12-27 16:54:39 +0300

    add SortSets.py

--- Python/.Python/testing.py
+++ Python/.Python/testing.py
@@ -1,10 +1,15 @@
-list = [1, 21 ,3, -46, 53, -6, 117]
-size = len(list)
-print(list)
-for i in range(size-1):
-    min_position = i
-    for j in range(i, size):
-        if list[j] < list[min_position]:
-            min_position = j
-    list[i], list[min_position] = list[min_position], list[i]
-print(list)
+var1 = '5 4'       # количество элементов первого и второго множества
+var2 = '10 30 5 7 9' # элементы первого множества через пробел
+var3 = '2 30 4 5'   # элементы второго множества через пробел
+
+def createset(arg):
+    newset = set()
+    for i in arg:
+        if i != ' ':
+            newset.add(i)
+    return newset
+list1 = var2.split(' ')
+list2 = var3.split(' ')
+set1 = createset(list1)
+set2 = createset(list2)
+print(set2.intersection(set1))
