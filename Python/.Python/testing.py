bdf73ec0e20a3600b27d79c43bbcdafac232dea9
Sublustrum007 <Sublustrum007@yandex.ru>
2024-01-18 14:39:55 +0300

    18.01.2024 добавил решение ариф.прогресси

--- Python/.Python/testing.py
+++ Python/.Python/testing.py
@@ -1,15 +1,32 @@
-var1 = '5 4'       # количество элементов первого и второго множества
-var2 = '10 30 5 7 9' # элементы первого множества через пробел
-var3 = '2 30 4 5'   # элементы второго множества через пробел
+# data = [int(i) for i in input("Введите кол-во ягод: ").split()]
+# max_summa = 0
+# for i in range(len(data)):
+#     if max_summa < data[i - 1] + data[i] + data[(i + 1) % len(data)]:
+#         max_summa = data[i - 1] + data[i] + data[(i + 1) % len(data)]
+# print(max_summa)
 
-def createset(arg):
-    newset = set()
-    for i in arg:
-        if i != ' ':
-            newset.add(i)
-    return newset
-list1 = var2.split(' ')
-list2 = var3.split(' ')
-set1 = createset(list1)
-set2 = createset(list2)
-print(set2.intersection(set1))
+# Автотест задача 2, семинар 6 (Повторение списков)
+
+# Заполните массив элементами арифметической прогрессии. Её первый элемент a1 , 
+# разность d и количество элементов n будет задано автоматически. Формула для 
+# получения n-го члена прогрессии: an = a1 + (n-1) * d.
+
+# a1 = 2
+# d = 3
+# n = 4
+
+# # 2     (a1 + (n - 1) * d)
+# # 5     (a1 + (n - 1) * d)
+# # 8     (a1 + (n - 1) * d)
+# # 11    (a1 + (n - 1) * d)
+
+# massiv = []
+# def arif(a1, d, n):
+#     if n == 0:
+#         return(massiv)
+#     an = a1 + (n - 1) * d
+#     massiv.insert(0, an)
+#     arif(a1, d, n - 1)
+# arif(a1, d, n)
+# for i in range(len(massiv)):
+#     print(f'{massiv[i]}')
